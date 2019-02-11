bea9a34ed2d95babae39629e86367c6d7cc78622
franciscotis <franncisco.p@gmail.com>
2019-02-10 22:12:51 -0200

    Criação da FSM

--- verilog/arbitro.v
+++ verilog/arbitro.v
@@ -14,29 +14,7 @@ module arbitro (
 	input  [31:0] writedata,	//(Sem uso) Palavra(4 bytes) de escrita
 	output [31:0] readdata,		//([10:0] em uso) Número do sensor (000 para erro de colisão) e byte recebido
 	
-	//Entradas e saídas do uart RS232
-	input  rx,	//Pino externo de entrada
-	output tx	//Pino externo de saída
 );
-
-//Declaração do módulodo receptor UART
-wire [15:0] o_data_rx;	//2 bytes recebidos
-wire o_done_rx;		//Habilitado quando os 2 bytes são recebidos
-uart_rx UART_RX (.clock(clock), .reset(~reset), .rx(rx), .data(o_data_rx), .done(o_done_rx));
-
-
-//Declaração do módulo emissor UART
-wire [7:0] i_data_tx;	//Número do sensor
-wire i_enable_tx;	//Habilita o envio do byte
-wire o_active_tx;	//Habilitado enquanto o byte está sendo enviado
-wire o_done_tx;		//Habilitado quando o byte foi enviado
-uart_tx UART_TX (.clock(clock), .reset(~reset), .data(i_data_tx), .enable(i_enable_tx), 
-		.active(o_active_tx), .done(o_done_tx), .tx(tx));
-
-
-//Declaração do módulo de vericação do CRC
-wire o_result_crc;	//Resultado da verficação
-checksum CHECKSUM (.data(o_data_rx[7:0]), .crc(o_data_rx[15:8]), .result(o_result_crc));
 
 
 //Declaração da FSM de controle do árbitro
